a0acade5dd667c1d4ced5be1002c006470bd4d31
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2019-10-19 17:56:42 -0500

    Preparing background.js to pass messages to content.js

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,1 +1,14 @@
 // background.js
+
+// Called when the user clicks on the browser action
+chrome.browserAction.onClicked.addListener(
+    function(tab) {
+        // For now, send a toggle message to content.js
+        // toggle message will be used to start and stop/print new recording
+        console.log("Sending toggle");
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            var activeTab = tabs[0]
+            chrome.tabs.sendMessage(activeTab.id, {"message": "toggle"})
+        });
+    }
+);
