c605125bc171c30d28a9cc3631472aa1e38e34dc
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2019-10-19 17:30:15 -0500

    Preparing to add recorder to page

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -1,1 +1,44 @@
 // content.js
+
+chrome.runtime.onMessage.addListener(
+    function() {
+        // Something
+    }
+);
+
+window.onload = function() {
+    var recorder = new Recorder();
+    recorder.init();
+}
+
+var Recorder = (function() {
+    let isRecording,
+    recordingList = [];
+
+    function init() {
+        console.log("Recorder created");
+        return this;
+    };
+
+    return {
+        init: init
+    };
+});
+
+var Recording = (function() {
+
+    let eventList = [];
+
+    function addEvent(event) {
+        eventList.push(event);
+    };
+
+    function getEventList() {
+        return eventList;
+    };
+
+    return {
+        addEvent: addEvent,
+        getEventList: getEventList
+    };
+});
