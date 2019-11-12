e7ca514cc95cb6fbfcf17c69816dbed609eda0d7
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2019-11-11 20:35:26 -0600

    Added more fields and behavior to the test page

--- testPage.js
+++ testPage.js
@@ -1,23 +1,48 @@
 window.onload = function() {
-    let listenerTypes = new Set();
+    console.log("Loaded testPage.js");
+    toggleListeners();
+}
+
+const listenerTypes = new Set();
     listenerTypes.add("click");
     listenerTypes.add("change");
-    // listenerTypes.add("keyup");
-    function func(node) {
-        if (node.childElementCount === 0) {
+    // listenerTypes.add("keypress");
+    listenerTypes.add("focus");
+
+let listenersActive = false;
+
+const toggleListeners = function() {
+    if(listenersActive) {
+        toggleHelper(document,false);
+        console.log("Listeners deactivated");
+    } else {
+        toggleHelper(document, true);
+        console.log("Listeners Activated");
+    }
+    listenersActive = !listenersActive;
+}
+
+const listen = function(event) {
+    console.log("Event triggerred");
+    console.log(event.path);
+    console.log(event);
+}
+
+const toggleHelper = function(node, setOn) {
+    if (node.childElementCount === 0) {
+        if(setOn) {
             listenerTypes.forEach(function(type) {
-                node.addEventListener(type, function(event) {
-                    console.log(type + " event triggerred");
-                    console.log(event.path);
-                    console.log(event);
-                })
+                node.addEventListener(type, listen);
             })
         } else {
-            for(let i=0; i<node.childElementCount; i++) {
-                func(node.children[i]);
-            }
+            listenerTypes.forEach(function(type) {
+                node.removeEventListener(type, listen)
+            })
+        }
+        
+    } else {
+        for(let i=0; i<node.childElementCount; i++) {
+            toggleHelper(node.children[i], setOn);
         }
     }
-    func(document);
-    console.log("Loaded testPage.js");
 }
