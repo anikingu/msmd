102f866c0125a9f9fd99adb2c4292b9ac7ceb7ec
Raymond <soanikingu@gmail.com>
2021-10-03 22:10:40 -0500

    Reworked main process to be encapsulated for better control over windows

--- record/src/main.js
+++ record/src/main.js
@@ -6,54 +6,93 @@ const { ScriptBuilder, StepType } = require('./script-builder');
 /* Used for hot reloading app during development*/
 try {
     require('electron-reloader')(module);
-} catch {}
+} catch { }
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
     app.quit();
 }
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 2000, 
-        height: 825,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
-        }
+const MainProcess = function () {
+    let mainWindow;
+    let monitoredWindow;
+    let builder;
+
+    const init = () => {
+        createMainWindow();
+        createMonitoredWindow();
+        createBuilder('https://wikipedia.org');
+    }
+
+    const createMainWindow = () => {
+        mainWindow = new BrowserWindow({
+            width: 2000,
+            height: 825,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+                enableRemoteModule: true
+            }
+        });
+        mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+        // win.loadFile('./console/console.html')
+        mainWindow.openDevTools();
+    }
+    const createMonitoredWindow = () => {
+        monitoredWindow = new BrowserWindow({
+            x: 450,
+            y: 90,
+            width: 1500,
+            height: 700,
+            parent: mainWindow,
+            kiosk: true,
+            fullscreen: false,
+            fullscreenable: false,
+            // frame: false,
+            autoHideMenuBar: true,
+            hasShadow: false,
+            webPreferences: {
+                preload: path.join(__dirname, 'renderer/event-register.js')
+            }
+        });
+        monitoredWindow.webContents.loadURL('https://www.wikipedia.org');
+        // view.webContents.openDevTools();
+
+    };
+
+    const createBuilder = (url) => {
+        builder = ScriptBuilder(url);
+    }
+
+    ipcMain.on('click-message', (event, eventDto) => {
+        console.log('Received message');
+        console.log(eventDto);
+        builder.addStep(StepType.INTERACT, eventDto, 'click');
     });
-    win.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-    // win.loadFile('./console/console.html')
-    win.openDevTools();
-    // const view = new BrowserWindow({
-    //     x: 450,
-    //     y: 90,
-    //     width: 1500,
-    //     height: 700,
-    //     parent: win,
-    //     kiosk: true,
-    //     fullscreen: false,
-    //     fullscreenable: false,
-    //     // frame: false,
-    //     autoHideMenuBar: true,
-    //     hasShadow: false,
-    //     webPreferences: {
-    //         preload: path.join(__dirname, 'renderer/event-register.js')
-    //     }
-    // });
-    // win.setBrowserView(view);
-    // view.webContents.loadURL('https://www.wikipedia.org');
-    // view.webContents.openDevTools();
-    
-};
+
+    ipcMain.on('input-changed-message', (event, eventDto) => {
+        console.log('Input Received');
+        console.log(eventDto);
+        builder.addStep(StepType.INTERACT, eventDto, "change");
+    });
+
+    ipcMain.on('create-file', (event, eventDto) => {
+        builder.save();
+    });
+
+    return {
+        init: init,
+        createMainWindow: createMainWindow,
+        createMonitoredWindow: createMonitoredWindow
+    }
+}();
 
 app.whenReady().then(() => {
-    createWindow();
+    MainProcess.init();
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
+            MainProcess.createMainWindow();
         }
     });
 });
@@ -66,20 +105,3 @@ app.on('window-all-closed', () => {
 });
 
 
-let builder = ScriptBuilder('https://wikipedia.org');
-
-ipcMain.on('click-message', (event, eventDto) => {
-    console.log('Received message');
-    console.log(eventDto);
-    builder.addStep(StepType.INTERACT, eventDto, 'click');
-});
-
-ipcMain.on('input-changed-message', (event, eventDto) => {
-    console.log('Input Received');
-    console.log(eventDto);
-    builder.addStep(StepType.INTERACT, eventDto, "change");
-});
-
-ipcMain.on('create-file', (event, eventDto) => {
-    builder.save();
-});
